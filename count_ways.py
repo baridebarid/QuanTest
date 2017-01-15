f8cce6307b8c222f5639a14a2fe12c3f3886c801
baridebarid <baridebarid@users.noreply.github.com>
2017-01-15 15:14:17 +0200

    Update count_ways.py

--- count_ways.py
+++ count_ways.py
@@ -53,7 +53,7 @@ def read_array(array_len):
     return array
 
 
-def clean_input():
+def clean_input_array():
     array = read_array(read_array_length())
     for ind, el in enumerate(array):
         if el > RANGE:
@@ -68,11 +68,12 @@ def clean_input():
     return array
 
 
-def count_ways_another_way(array):
-    """ Counting number of  ways to break input array to sub arrays of equal sum another way """
+def count_ways(array):
+    """ Counting number of  ways to break input array to subarrays of equal sum """
 
     print "\narray", array
     total_sum = sum(array)
+    result = 0
 
     if total_sum % 3 == 0:  # Checking is it possible to get equal sums
         array_len = len(array)
@@ -87,7 +88,7 @@ def count_ways_another_way(array):
 
         for i in range(array_len - 2, -1, -1):
             count[i] += count[i + 1]
-        result = 0
+
         sub_sum = 0
         for i in range(array_len - 2):
             sub_sum += array[i]
@@ -95,16 +96,21 @@ def count_ways_another_way(array):
                 result += count[i + 2]
     else:
         print "Sum will never be equal. Just give up."
-        result = 0
 
     print result, "way(s)"
     return result
 
 
+def test_count_ways():
+    assert count_ways([1, 2, 3, 0, 3]) == 2
+    assert count_ways([0, 1, -1, 0]) == 1
+    assert count_ways([0, 0, 0, 0, 0, 0, 0, 0, 0]) == 28
+    assert count_ways([1, 1, 1, 1, 1, 1]) == 1
+    assert count_ways([3, 3, -3, 3, 3]) == 3
+
 if __name__ == '__main__':
 
-    count_ways_another_way([1, 2, 3, 0, 3])
-    count_ways_another_way([0, 1, -1, 0])
-    count_ways_another_way([0, 0, 0, 0, 0, 0, 0, 0, 0])
-    count_ways_another_way([1, 1, 1, 1, 1, 1])
-    count_ways_another_way([3, 3, -3, 3, 3])
+    test_count_ways()
+    array = clean_input_array()
+    count_ways()
+
